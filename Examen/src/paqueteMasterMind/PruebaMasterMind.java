cc3ec862171bc1d6aff4a882707a8dc499741063
Alaynchus <80107470+Alaynchus@users.noreply.github.com>
2021-05-21 13:15:18 +0200

    Update PruebaMasterMind.java

--- Examen/src/paqueteMasterMind/PruebaMasterMind.java
+++ Examen/src/paqueteMasterMind/PruebaMasterMind.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class PruebaMasterMind {
 
 	public static void main (String[] args) throws IOException {
-
+		System.out.println("Editado en el repositorio");
 		MasterMind mm1 = new MasterMind();
 		MasterMind mm2 = new MasterMind();
 		
